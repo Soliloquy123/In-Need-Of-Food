2c1ae7b4b8477e9564d2e2a9b3b5c7a033733dab
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-03-30 12:40:52 +0100

    more dialougue coide done

--- In Need Of Food/Assets/Scripts/Dialog/Dialogue Trigger.cs
+++ In Need Of Food/Assets/Scripts/Dialog/Dialogue Trigger.cs
@@ -7,6 +7,9 @@ public class DialogueTrigger : MonoBehaviour
     [Header("Visual Cue")]
     [SerializeField] private GameObject visualCue;
 
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
     private bool npcInRange;
     private void Awake()
     {
@@ -14,13 +17,35 @@ public class DialogueTrigger : MonoBehaviour
         visualCue.SetActive(false);
     }
 
-    private void OnTriggerEnter2D(Collider2D colliider)
+    private void Update()
     {
-        
+        if (npcInRange)
+        {
+            visualCue.SetActive(true);
+            if (InputManager.GetInstance().GetInteractPressed())
+            {
+                Debug.Log(inkJSON.text); 
+            }
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D colider)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        
+        if (collider.gameObject.tag == "NPC")
+        {
+            npcInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "NPC")
+        {
+            npcInRange = false;
+        }
     }
 }
