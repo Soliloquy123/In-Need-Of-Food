b7cb0370961b8bfd6eee59f046d5bde6e191c980
EmotionalInteractive2022 <97882975+EmotionalInteractive2022@users.noreply.github.com>
2022-05-22 21:53:51 +0100

    halfway done tags

--- Assets/Scripts/Dialog/DialogueManager.cs
+++ Assets/Scripts/Dialog/DialogueManager.cs
@@ -30,6 +30,10 @@ public class DialogueManager : MonoBehaviour
 
     private static DialogueManager instance;
 
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+    private const string LAYOUT_TAG = "layout";
+
     private void Awake()
     {
         if (instance != null)
@@ -85,10 +89,45 @@ public class DialogueManager : MonoBehaviour
             dialogueText.text = currentStory.Continue();
             // display choices, if any, for this dialogue line 
             DisplayChoices();
+            //handles the tags
+            HandleTags(currentStory.currentTags);
         }
         else
         {
             ExitDialogueMode();
+        }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        // Loop throught each tag and handl it accordingly
+        foreach (string tag in currentTags)
+        {
+            //parse the tag
+            string[] splitTag = tag.Split(':');
+            {
+                Debug.LogError("Tag could not be appropiately parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            //handles the tag
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    Debug.Log("speaker=" + tagValue);
+                    break;
+                case PORTRAIT_TAG:   
+                    Debug.Log("portrait=" + tagValue);
+                    break; 
+                case LAYOUT_TAG:    
+                    Debug.Log("speaker=" + tagValue);
+                    break;    
+                default:
+                    Debug.LogWarning("Tag came in but is not currently bing handled:" + tag);
+                    break;
+
+            }
         }
     }
 
